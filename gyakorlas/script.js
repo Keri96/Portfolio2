e2f5292cf8f05b7265c31d5938c1e07c2e1209a1
Keri96 <kerikanyi@gmail.com>
2023-08-29 14:24:44 +0200

    Input fields cleared after submit

--- gyakorlas/script.js
+++ gyakorlas/script.js
@@ -5,6 +5,12 @@ function sendMail() {
     message: document.getElementById("message").value,
   };
   emailjs.send("keri_service", "template_0zirslj", params).then(function (res) {
-    alert("Success" + res.status);
+    alert("Email sent successfully");
   });
+}
+
+function clearInputFields() {
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("message").value = "";
 }
